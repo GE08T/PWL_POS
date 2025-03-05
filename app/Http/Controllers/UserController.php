16110e18f851801e93b7e6e88dc8b6237749d71c
GE08T <gaerthamgk@gmail.com>
2025-03-05 09:02:33 +0700

    Praktikum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,15 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        // $user = UserModel::all();
-        // return view('user', ['user' => $user]);
+        // $user = UserModel::find(1);
         
-        $data = [
-            'level_id' => '2',
-            'username' => 'manager-tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-        
-        $user = UserModel::all();
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        $user = UserModel::findOr(20, ['username' , 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
