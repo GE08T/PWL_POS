350aa45f28bce6a6591ee33604db757301d4d6ca
GE08T <gaerthamgk@gmail.com>
2025-04-13 09:07:12 +0700

    Pertemuan 7_Praktikum 2 - Implementasi Authorizaton di Laravel dengan Middleware

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [WelcomeController::class, 'index']);
 
 Route::pattern('id', '[0-9]+');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
@@ -28,6 +27,7 @@ Route::post('/login', [AuthController::class, 'postlogin']);
 Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
 Route::middleware(['auth'])->group(function() {
+    Route::get('/', [WelcomeController::class, 'index']);
     Route::group(['prefix' => 'user'], function() {
         Route::get('/', [UserController::class, 'index']);
         Route::post('/list', [UserController::class, 'list']);
@@ -41,18 +41,19 @@ Route::middleware(['auth'])->group(function() {
         Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
         Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);
     });
-    
-    Route::group(['prefix' => 'level'], function() {
-        Route::get('/', [LevelController::class, 'index']);
-        Route::post('/list', [LevelController::class, 'list']);
-        //Ajax
-        Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
-        Route::post('/ajax', [LevelController::class, 'store_ajax']);
-        Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);
-        Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
-        Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
-        Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);
-        Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
+    Route::middleware(['authorize:ADM'])->group(function() {
+        Route::group(['prefix' => 'level'], function() {
+            Route::get('/', [LevelController::class, 'index']);
+            Route::post('/list', [LevelController::class, 'list']);
+            //Ajax
+            Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
+            Route::post('/ajax', [LevelController::class, 'store_ajax']);
+            Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);
+            Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
+            Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
+            Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);
+            Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
+        });
     });
     
     Route::group(['prefix' => 'kategori'], function() {
